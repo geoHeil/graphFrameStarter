ed001f24e039ec61f8dfbecce2fb803f4a38574c
geoHeil <georg.kf.heiler@gmail.com>
2017-01-31 19:18:43 +0100

    update

--- README.md
+++ README.md
@@ -14,17 +14,16 @@ Getting started with graph-frames I am not sure how to move forward. Looking for
 The following nodes are present:
 
 ```
-+---+-------+-----+
-| id|   name|fraud|
-+---+-------+-----+
-|  a|  Alice|    1|
-|  b|    Bob|    0|
-|  c|Charlie|    0|
-|  d|  David|    0|
-|  e| Esther|    0|
-|  f|  Fanny|    0|
-|  g|  Gabby|    0|
-+---+-------+-----+
+---+---------+-----+
+| id|     name|fraud|
++---+---------+-----+
+|  a|    Alice|    1|
+|  b|      Bob|    0|
+....
+|  f|    Fanny|    0|
+|  g|    Gabby|    0|
+|  h|Fraudster|    1|
++---+---------+-----+
 ```
 
 and edges
@@ -32,26 +31,59 @@ and edges
 +---+---+------------+
 |src|dst|relationship|
 +---+---+------------+
-|  a|  b|           A|
-|  b|  c|           B|
-|  c|  b|           B|
-|  f|  c|           B|
-|  e|  f|           B|
-|  e|  d|           A|
-|  d|  a|           A|
-|  a|  e|           A|
+|  a|  b|        call|
+|  b|  c|         sms|
+....
+|  a|  h|        call|
+|  f|  h|        call|
 +---+---+------------+
 ```
 
-## ongoing discussions
+
+## direct friends
+
+> TODO check: I did not cache the graph but still make sure spark is not caching the results, 
+> otherwise comparing them one after the other has no meaning
+
+**naive calculation**
+
+Using a naive method (join) the calculation can be performed.
+It takes around 4 seconds on my MacBook. It is measured including the `df.show` statement, but still even if it takes one
+second for this amount of vertices, that is very slow.
+
+But:
+> this is a false friend. This method will ignore vertices not present as outgoing.
+
+**improved calculation**
+
+Using sparks RDD API and a statCounter can speed up the computation by a lot. Now it takes only 007 seconds.
+For such a small graph the DAG does not really look simpler. But the timing shows the advantage.
+ 
+
+**naive method with different types of edges**
+
+![DAG of naive join variant](graphPercentageWithJoins.jpg "naive and slow joins DAG")
+
+**improved method with different types of edges**
+
+
+**pregel API: Message passing via AggregateMessages**
+
+### ongoing discussions
+
+  - http://stackoverflow.com/questions/41946947/spark-graphframes-stateful-motif
+  - http://stackoverflow.com/questions/41908966/spark-graph-efficient-joins
   - https://forums.manning.com/posts/list/39971.page
+  - https://forums.manning.com/posts/list/40095.page
 
-## some graph-frames tutorials
+### some graph-frames tutorials
 
+  - http://graphframes.github.io/user-guide.html
   - https://www.mapr.com/blog/using-spark-graphframes-analyze-facebook-connections
+  - https://www.mapr.com/blog/how-get-started-using-apache-spark-graphx-scala
   - https://www.youtube.com/watch?v=zx9KI3DsZss
   
-## coll libraries
+### cool libraries
   - https://github.com/sparkling-graph/sparkling-graph
   
 ## visualization
--- src/main/scala/myOrg/ExampleSQL.scala
+++ src/main/scala/myOrg/ExampleSQL.scala
@@ -2,10 +2,11 @@ package myOrg
 
 import org.apache.log4j.Logger
 import org.apache.spark.SparkConf
-import org.apache.spark.graphx.Graph
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{Column, DataFrame, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.util.StatCounter
 import org.graphframes.GraphFrame
+import org.graphframes.lib.AggregateMessages
 
 object ExampleSQL extends App {
 
@@ -30,104 +31,144 @@ object ExampleSQL extends App {
     ("d", "David", 0),
     ("e", "Esther", 0),
     ("f", "Fanny", 0),
-    ("g", "Gabby", 0)
+    ("g", "Gabby", 0),
+    ("h", "Fraudster", 1)
   )).toDF("id", "name", "fraud")
   val e = spark.createDataFrame(List(
-    ("a", "b", "A"),
-    ("b", "c", "B"),
-    ("c", "b", "B"),
-    ("f", "c", "B"),
-    ("e", "f", "B"),
-    ("e", "d", "A"),
-    ("d", "a", "A"),
-    ("a", "e", "A")
+    ("a", "b", "call"),
+    ("b", "c", "sms"),
+    ("c", "b", "sms"),
+    ("f", "c", "sms"),
+    ("e", "f", "sms"),
+    ("e", "d", "call"),
+    ("d", "a", "call"),
+    ("d", "e", "sms"),
+    ("a", "e", "call"),
+    ("a", "h", "call"),
+    ("f", "h", "call")
   )).toDF("src", "dst", "relationship")
   val g = GraphFrame(v, e)
 
-  println(toGraphML(g))
-  val pw = new java.io.PrintWriter("myGraph.graphml")
-  pw.write(toGraphML(g))
-  pw.close
+  // TODO use nicer method to write files
+  // http://stackoverflow.com/questions/4604237/how-to-write-to-a-file-in-scala
+  // or https://github.com/pathikrit/better-files
+  // or https://github.com/scalaz/scalaz-stream
+    val pw = new java.io.PrintWriter("myGraph.graphml")
+    pw.write(toGraphML(g))
+    pw.close
 
-  g.vertices.show
-  g.edges.show
+  // connections of direct friends
+  val friends: DataFrame = g.find("(a)-[e]->(b)")
+  // 1) a-> b, direct, b fraud
+  // naive version ###############################################################################
+  val total = friends.groupBy($"a.id").count.withColumnRenamed("count", "totalCount")
+  val fraudDirect = friends.filter($"b.fraud" === 1).groupBy($"a.id").count.withColumnRenamed("count", "fraudCount")
+  // lets try it with join first, but already for this minimal dataset it is quite slow
+  time(total
+    .join(fraudDirect, Seq("id"), "left").
+    na.fill(0)
+    .withColumn("fraudPercentage", $"fraudCount" / $"totalCount")
+    .orderBy("id")
+    .show)
 
-  g.inDegrees.show
-  g.outDegrees.show
-  g.degrees.show
-  //  g.vertices.groupBy().max("fraud").show
+  // gwithout join, but fairly complex. TODO this is quite some code overhead. Could this be simplified (by a lot)?
+  // I know that the code handles multiple columns which is not really needed ... but anything else?
+  val fraudGroupDirect = friends.groupBy($"a.id", $"b.fraud").count
 
-  g.edges.filter("relationship = 'B'").count
+  val exploded = explode(array(
+    (Seq("id")).map(c =>
+      struct(lit(c).alias("k"), col(c).alias("v"))): _*
+  )).alias("level")
 
-  //  val g = examples.Graphs.friends
-  val motifs: DataFrame = g.find("(a)-[e]->(b); (b)-[e2]->(a)")
-  motifs.show
-  motifs.filter("b.fraud > 0").show
+  val long = fraudGroupDirect.select(exploded, col("fraud"))
 
-  val chain4 = g.find("(a)-[ab]->(b); (b)-[bc]->(c); (c)-[cd]->(d)")
-
-  // Query on sequence, with state (cnt)
-  //  (a) Define method for updating state given the next element of the motif.
-  def sumCall(cnt: Column, relationship: Column): Column = {
-    when(relationship === "A", cnt + 1).otherwise(cnt)
+  // http://stackoverflow.com/questions/41445571
+  val lookup = long.as[((String, String), Int)].rdd
+    // You can use prefix partitioner (one that depends only on _._1)
+    // to avoid reshuffling for groupByKey. Required only for distributed collection
+    .aggregateByKey(StatCounter())(_ merge _.toDouble, _ merge _)
+    .map { case ((c, v), s) => (c, (v, s)) }
+    .groupByKey
+    .mapValues(_.toMap)
+    .collectAsMap
+  val joinUDF = udf((newColumn: String, newValue: String) => {
+    lookup.get(newColumn).flatMap(_.get(newValue))
+      .fold(0.0) { case s: StatCounter => s.mean }
+  })
+  time(lookup.foldLeft(g.vertices) {
+    (currentDF, colName) => {
+      currentDF
+        .withColumn("directFraudulencyScore", when($"fraud" === 1, 1)
+          .otherwise(joinUDF(lit(colName._1), col(colName._1))))
+    }
   }
+    .orderBy("id")
+    .show)
 
-  //  (b) Use sequence operation to apply method to sequence of elements in motif.
-  //      In this case, the elements are the 3 edges.
-  val condition = Seq("ab", "bc", "cd").
-    foldLeft(lit(0))((cnt, e) => sumCall(cnt, col(e)("relationship")))
-  //  (c) Apply filter to DataFrame.
-  val chainWith2Friends2 = chain4.where(condition >= 2)
-  chainWith2Friends2.show
+  // ########################################################################################################
+  // now pregl message passing API: Message passing via AggregateMessages ##########################################
+  //  val g: GraphFrame = examples.Graphs.friends
+  val AM = AggregateMessages
 
-  // TODO compute mentioned fraud values values
-  // percentage of in and out degree for fraud for direct node and friends of friends
+  val msgToSrc = AM.dst("fraud")
+  val msgToDst = AM.src("fraud")
+  val agg = g.aggregateMessages
+    .sendToSrc(msgToSrc)  // send destination user's fraud to source
+    .sendToDst(msgToDst)  // send source user's fraud to destination
+    .agg(sum(AM.msg).as("summedFraud"))  // sum up ages, stored in AM.msg column
+  // TODO how to add in conditions / total and percentage / weights / type of connection
+  time(agg.show)
+  // ########################################################################################################
 
-  // compute shortest pats to fraud
-
-  // TODO access node
+  // TODO access node via proper XML writer
   // maybe https://github.com/apache/tinkerpop/blob/4293eb333dfbf3aea19cd326f9f3d13619ac0b54/gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/structure/io/graphml/GraphMLWriter.java is helpful
   // https://github.com/apache/tinkerpop/blob/4293eb333dfbf3aea19cd326f9f3d13619ac0b54/gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/structure/io/graphml/GraphMLTokens.java
-
-  toGraphML(g)
-  println(toGraphML(g))
-/// TODO improve writer as outlined by https://github.com/sparkling-graph/sparkling-graph/issues/8 and integrate there
+  /// TODO improve writer as outlined by https://github.com/sparkling-graph/sparkling-graph/issues/8 and integrate there
   def toGraphML(g: GraphFrame): String =
-    s"""
-       |<?xml version="1.0" encoding="UTF-8"?>
-       |<graphml xmlns="http://graphml.graphdrawing.org/xmlns"
-       |         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-       |         xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
-       |         http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
-       |
-       |  <key id="v_name" for="node" attr.name="name" attr.type="string"/>
-       |  <key id="v_fraud" for="node" attr.name="fraud" attr.type="int"/>
-       |  <key id="e_edgeType" for="edge" attr.name="edgeType" attr.type="string"/>
-       |  <graph id="G" edgedefault="directed">
-       |${
-      g.vertices.map {
-        case Row(id, name, fraud) =>
-          s"""
-             |      <node id="${id}">
-             |         <data key = "v_name">${name}</data>
-             |         <data key = "v_fraud">${fraud}</data>
-             |      </node>
+  s"""
+     |<?xml version="1.0" encoding="UTF-8"?>
+     |<graphml xmlns="http://graphml.graphdrawing.org/xmlns"
+     |         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+     |         xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
+     |         http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
+     |
+     |  <key id="v_name" for="node" attr.name="name" attr.type="string"/>
+     |  <key id="v_fraud" for="node" attr.name="fraud" attr.type="int"/>
+     |  <key id="e_edgeType" for="edge" attr.name="edgeType" attr.type="string"/>
+     |  <graph id="G" edgedefault="directed">
+     |${
+    g.vertices.map {
+      case Row(id, name, fraud) =>
+        s"""
+           |      <node id="${id}">
+           |         <data key = "v_name">${name}</data>
+           |         <data key = "v_fraud">${fraud}</data>
+           |      </node>
            """.stripMargin
-      }.collect.mkString.stripLineEnd
-    }
-       |${
-      g.edges.map {
-        case Row(src, dst, relationship) =>
-          s"""
-             |      <edge source="${src}" target="${dst}">
-             |      <data key="e_edgeType">${relationship}</data>
-             |      </edge>
+    }.collect.mkString.stripLineEnd
+  }
+     |${
+    g.edges.map {
+      case Row(src, dst, relationship) =>
+        s"""
+           |      <edge source="${src}" target="${dst}">
+           |      <data key="e_edgeType">${relationship}</data>
+           |      </edge>
            """.stripMargin
-      }.collect.mkString.stripLineEnd
-    }
-       |  </graph>
-       |</graphml>
+    }.collect.mkString.stripLineEnd
+  }
+     |  </graph>
+     |</graphml>
   """.stripMargin
+
+  private def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block
+    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed: " + ((t1 - t0).toDouble / 1000000000) + "sec")
+    result
+  }
 
   spark.stop
 
